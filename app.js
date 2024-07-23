1f18a63b35aed5747772f0ac1849204f58752650
apple <apple@apples-MacBook-Pro.local>
2024-07-23 17:57:18 +0530

    test case pass for \n and ,

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ export const add = (data) => {
   if (data.length == 0) return 0;
   else if (data.length == 1) return parseInt(data);
   else {
-    let nums = data.split(",").map((num) => parseInt(num));
+    const regex = /\d+/g;
+    let nums = data.match(regex).map((num) => parseInt(num));
     return nums.reduce((num, sum) => {
       return num + sum;
     }, 0);
