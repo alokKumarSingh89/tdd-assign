a17fb4a7ab05a9c62d7be7f2fb74268b630abf54
apple <apple@apples-MacBook-Pro.local>
2024-07-23 17:51:50 +0530

    add number when the comma seperated

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 export const add = (data) => {
   if (data.length == 0) return 0;
   else if (data.length == 1) return parseInt(data);
+  else {
+    let nums = data.split(",").map((num) => parseInt(num));
+    return nums.reduce((num, sum) => {
+      return num + sum;
+    }, 0);
+  }
 };
--- tests/app.test.js
+++ tests/app.test.js
@@ -6,8 +6,13 @@ describe("Sum of number from given string", () => {
     const sum = add("");
     expect(sum).toBe(0);
   });
-  it("Return the number when string have 1 number", () => {
+  it("Return the number when string have single number", () => {
     const sum = add("1");
     expect(sum).toBe(1);
+  });
+  it("Return the sum of number when they are comma seperate", () => {
+    let sum = add("1,2");
+    expect(sum).toBe(3);
+    expect(add("1,2,3,4")).toBe(10);
   });
 });
