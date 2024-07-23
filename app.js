3971b9e6c0d9b1ee0dca0b7c9cfb44876e179bb6
apple <apple@apples-MacBook-Pro.local>
2024-07-23 18:11:47 +0530

    exception test case passed

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ export const add = (data) => {
   if (data.length == 0) return 0;
   else if (data.length == 1) return parseInt(data);
   else {
-    const regex = /\d+/g;
-    let nums = data.match(regex).map((num) => parseInt(num));
+    const regex = /[-]{0,1}[\d]*/g;
+    let nums = data
+      .match(regex)
+      .map((num) => parseInt(num))
+      .filter((num) => !isNaN(num));
+    let negativeNumber = nums.filter((num) => num < 0);
+    if (negativeNumber.length > 0) {
+      throw new Error(
+        `negative numbers not allowed ${negativeNumber.join(",")}`
+      );
+    }
     return nums.reduce((num, sum) => {
       return num + sum;
     }, 0);
